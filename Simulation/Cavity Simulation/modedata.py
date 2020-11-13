1f713b063022359343163bbb9a32e2daed135014
thomas-rocke <45517493+dune-ruler@users.noreply.github.com>
2020-11-13 15:21:40 +0000

    Add Laguerre Mode Functionality
    Laguerre Modes approximately added, still some issues preventing exact form

--- Simulation/Cavity Simulation/modedata.py
+++ Simulation/Cavity Simulation/modedata.py
@@ -1,6 +1,7 @@
 import os
 from Gaussian_Beam import Superposition
 from Gaussian_Beam import Gaussian_Mode
+import numpy as np
 
 def saveData(superposList, fname, path=None):    
     if(path==None):
@@ -25,3 +26,29 @@ def loadData(fname, path=None):
        superText = np.genfromtxt(f, delimiter='\n', dtype=str)
    supers = [eval(superposition) for superposition in superText]
    return supers
+
+def fact(x):
+    res = 1
+    if x != 0:
+        for i in range(x):
+            res *= i + 1
+    return res
+
+def choose(n, r):
+    return fact(n)/(fact(r)*fact(n-r))
+
+
+def makeLaguerre(p, m):
+    x=[]
+    for q in range(p + 1):
+        for s in range(int((m)/2) + 1):
+            frac = ((fact(2*(q-s) + m)*fact(2*(p-q+s)))/(2**(2*p + m - 1) * fact(p) * fact(p+m) * (1 + choose(0, m))))**0.5
+            y = Gaussian_Mode(2*(q - s) + m, 2*(p - q + s))
+            y.amplitude = (-1)**(s+p) * choose(p, q) * choose(m, 2*s) * frac
+            x.append(y)
+
+    return Superposition(x)
+
+
+y = makeLaguerre(3, 4)
+y.show()
