d782978af8eb3608348c1f403cee6b3ef9aa3182
Ninstet <jxw1025@student.bham.ac.uk>
2020-11-15 17:34:26 +0000

    Fix merge error

--- Simulation/Cavity Simulation/modedata.py
+++ Simulation/Cavity Simulation/modedata.py
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-import os
-from Gaussian_Beam import Superposition, Hermite, Laguerre
-import Gaussian_Beam as gauss
-import numpy as np
-
-def saveData(superposList, fname, path=None):    
-    if(path==None):
-        path = os.getcwd() + r'\Simulation\Cavity Simulation\Data'
-    file = path + os.sep + fname
-
-    superposStrings = [repr(superpos) + '\n' for superpos in superposList]
-    with open(file, 'w') as f:
-        f.writelines(superposStrings)
-        
-
-
-
-
-def loadData(fname, path=None):
-   import numpy as np
-   if(path==None):
-        path = os.getcwd() + r'\Simulation\Cavity Simulation\Data'
-   file = path + os.sep + fname
-    
-   with open(file, 'r') as f:
-       superText = np.genfromtxt(f, delimiter='\n', dtype=str)
-   supers = [eval(superposition) for superposition in superText]
-   return supers
-
-
-
-
-y = Laguerre(2, 1)
-y.show()
-x = Hermite(0, 0)
-#y = Hermite(0, 2)
-sup = Superposition([x, y])
-sup.show()
-y.show()
-=======
 import os
 from Gaussian_Beam import Superposition, Hermite, Laguerre
 import Gaussian_Beam as gauss
@@ -79,4 +38,3 @@ sup = Superposition([x, y])
 #sup.show()
 #y.show()
 print(sup.mode_matrix)
->>>>>>> 9cbb5b13a9e3eae7fa8457938f5b61bbf34eafd9
