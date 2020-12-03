844f2110d2b48e6d8b9f868ee77ed52bedb77fb0
thomas-rocke <45517493+dune-ruler@users.noreply.github.com>
2020-12-03 19:19:36 +0000

    Add in method of quantifying an error on predicted mode amplitudes

--- System/DataTransfer.py
+++ System/DataTransfer.py
@@ -1,6 +1,11 @@
 import matplotlib.image as im
 import numpy as np
 import os
+import random
+import sys
+sys.path.insert(1, '../Simulation/Cavity Simulation') # Move to directory containing simulation files
+from Gaussian_Beam import *
+from ML_Identification import *
 
 def readImage(fname, dir):
     #Opens the .png image file with name fname in directory dir and reads the data
@@ -30,3 +35,49 @@ def readCSV(fname, dir, headers=None, sep=',', skiprows=0):
             for j in range(len(headers)):
                 d[headers[j]] = raw_data[i+1, j]
             dict_list[i] = d
+
+
+def get_model_error(model, data_object, test_percent=0.1):
+    '''
+    Tests the accuracy of the model from data contained within data_object
+    
+    Assumes a gaussian resultant error through the Central Limit Theorem
+    
+    Tests on test_percent of the data given in the data_object
+    '''
+    test_number = test_percent * len(data_object.combs) # Number of superpositions to test
+    test_data = random.sample(data_object.combs, test_number) # Select superpositions randomly from combs
+    test_data = np.array([data_object.randomise_amp_and_phase(i) for i in test_data]) # Randomise all amps and phases
+    
+    model_predictions = np.array([model.predict(data.superpose()) for data in test_data]) # Predict superpositions through models
+    
+    
+    test_amps = np.array([[mode.amplitude for mode in sup] for sup in test_data])
+    model_amps = np.array([[mode.amplitude for mode in sup] for sup in model_predictions])
+    
+    amp_err = (np.sum([(model_amps[i] - test_amps[i])**2 for i in range(test_number)])/(test_number - 1))**0.5 # Predicts error assuming error is exual throughout multivariate space
+    
+    return amp_err
+
+max_order = 3
+number_of_modes = 3
+amplitude_variation = 0.2
+phase_variation = 0.2
+noise_variation = 0.0
+exposure = (0.0, 1.0)
+repeats = 50
+
+# train_and_save(max_order, number_of_modes, amplitude_variation, phase_variation, noise_variation, exposure, repeats)
+
+# train_and_save(3, 3, 0.2, 0.0, 50)
+# train_and_save(4, 3, 0.2, 0.0, 50)
+# train_and_save(5, 3, 0.2, 0.0, 50)
+
+model = ML(max_order, number_of_modes, amplitude_variation, phase_variation, noise_variation, exposure, repeats)
+model.load()
+
+data = Generate_Data(max_order, number_of_modes, amplitude_variation, phase_variation, noise_variation, exposure)
+
+print(get_model_error(model, data, 0.5))
+    
+    
