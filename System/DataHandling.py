Fixed phase prediction by normalising between 0 and 1 during training

System/DataHandling.py
@@ -216,7 +216,8 @@ class BasicGenerator(keras.utils.Sequence):
                  exposure: tuple = (0.0, 1.0),
                  repeats: int = 32,
                  batch_size: int = 64,
-                 resolution: int = 128):
+                 resolution: int = 128,
+                 cosine: bool = True):
         '''
         Initialise the class with the required complexity.
 
@@ -235,6 +236,7 @@ class BasicGenerator(keras.utils.Sequence):
         self.repeats = repeats
         self.batch_size = batch_size
         self.resolution = resolution
+        self.cosine = cosine
 
         cam = {"noise_variance":self.noise_variation,
                 "exposure_limits":self.exposure
@@ -264,13 +266,13 @@ class BasicGenerator(keras.utils.Sequence):
         '''
         Magic method for the repr() function.
         '''
-        return self.__class__.__name__ + f"({self.max_order}, {self.max_number_of_modes}, {self.amplitude_variation}, {self.phase_variation}, {self.noise_variation}, {self.exposure}, {self.repeats}, {self.batch_size}, {self.resolution})"
+        return self.__class__.__name__ + f"({self.max_order}, {self.max_number_of_modes}, {self.amplitude_variation}, {self.phase_variation}, {self.noise_variation}, {self.exposure}, {self.repeats}, {self.batch_size}, {self.resolution}, {self.cosine})"
 
     def copy(self):
         '''
         Copy this data generator.
         '''
-        return BasicGenerator(self.max_order, self.max_number_of_modes, self.amplitude_variation, self.phase_variation, self.noise_variation, self.exposure, self.repeats, self.batch_size, self.resolution)
+        return BasicGenerator(self.max_order, self.max_number_of_modes, self.amplitude_variation, self.phase_variation, self.noise_variation, self.exposure, self.repeats, self.batch_size, self.resolution, self.cosine)
 
     def __len__(self):
         '''
@@ -290,7 +292,8 @@ class BasicGenerator(keras.utils.Sequence):
         sups = [self.generate_superposition(comb) for comb in combs]
 
         X = np.array(self.get_inputs(*sups))[..., np.newaxis]
-        Y = np.array([[i.contains(j).amplitude for j in self.hermite_modes] + [np.cos(i.contains(j).phase) for j in self.hermite_modes] for i in sups])
+        if self.cosine: Y = np.array([[i.contains(j).amplitude for j in self.hermite_modes] + [np.cos(i.contains(j).phase) for j in self.hermite_modes] for i in sups]) # Use cos of phase
+        else: Y = np.array([[i.contains(j).amplitude for j in self.hermite_modes] + [(i.contains(j).phase + np.pi) / (2 * np.pi) for j in self.hermite_modes] for i in sups]) # Use normalised phase
 
         return X, Y
 