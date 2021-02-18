Fixed phase masking for Dataset class

System/DataHandling.py
@@ -292,7 +292,7 @@ class BasicGenerator(keras.utils.Sequence):
         sups = [self.generate_superposition(comb) for comb in combs]
 
         X = np.array(self.get_inputs(*sups))[..., np.newaxis]
-        Y = np.array([[i.contains(j).amplitude for j in self.hermite_modes] + [np.cos(i.contains(j).phase) if self.cosine else (i.contains(j).phase + np.pi) / (2 * np.pi) for j in self.hermite_modes] for i in sups]) # Use cos of phase
+        Y = np.array([[i.contains(j).amplitude for j in self.hermite_modes] + [np.cos(i.contains(j).phase) if self.cosine else (i.contains(j).phase + np.pi) / (2 * np.pi) for j in self.hermite_modes] for i in sups])
 
         return X, Y
 
@@ -482,7 +482,7 @@ class Dataset(keras.utils.Sequence):
                     mode.amplitude = 0
                     mode.phase = 0
         input_data = self.mode_processor.getImage(s.superpose())[..., np.newaxis] # Generate noise image
-        output_data = np.array([s.contains(j).amplitude for j in self.hermite_modes] + [np.cos(s.contains(j).phase) for j in self.hermite_modes])
+        output_data = np.array([s.contains(j).amplitude for j in self.hermite_modes] + [(s.contains(j).phase + np.pi) / (2 * np.pi) for j in self.hermite_modes])
 
         return input_data, output_data
 