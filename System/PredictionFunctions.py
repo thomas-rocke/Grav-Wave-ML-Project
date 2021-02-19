36b8535e11db06d931895254b825e596928cb5f5
thomas-rocke <45517493+dune-ruler@users.noreply.github.com>
2021-02-19 20:33:57 +0000

    Revert "Revert "Fixed Laguerre combining""
    This reverts commit 36f8ce4b866eb52b3fa2c7ab8ec84a49ffc4e32b.

--- System/PredictionFunctions.py
+++ System/PredictionFunctions.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from ImageProcessing import VideoProcessor
+from ImageProcessing import VideoProcessor, BaseProcessor, ModeProcessor
 from ML_Identification import ML
 from DataHandling import Dataset, BasicGenerator
+from Gaussian_Beam import Hermite, Superposition
 import Logger
 import os
 LOG = Logger.get_logger(__name__)
@@ -13,27 +14,43 @@ def visualise_video_predictions(video_file: str, model:ML):
     ax[0].set_title("Processed input image")
     ax[1].set_title("Image reconstruction of predictions")
     processor = VideoProcessor(video_file)
-    predictions = np.zeros((resolution[1], resolution[2], processor.frameCount))
+    predictions = np.zeros((resolution[0], resolution[1], processor.frameCount))
     for i in range(processor.frameCount):
-        try:
-            processed_frame = processor[i] # Get and process next frame
-            predicted_image = model.predict(processed_frame).superpose()
-            predictions[:,  :, i] = predicted_image
+        img = processor[i]
+        if i == 0:
+            params = processor.get_bounding_box(processor.toGreyscale(img/np.linalg.norm(img)))
+        processed_frame = processor.processImage(img, *params) # Get and process next frame
+        predicted_image = model.predict(processed_frame).superpose()
+        predictions[:,  :, i] = predicted_image
 
-            ax[0].imshow(processed_frame)
-            ax[1].imshow(predicted_image)
-            plt.pause(0.05)
-        except:
-            LOG.warn("Unexpected EOF")
+        ax[0].imshow(processed_frame)
+        ax[1].imshow(predicted_image)
+        plt.pause(0.05)
+        #LOG.warning("Unexpected EOF")
+    plt.show()
     return predictions
 
+def mode_sweep_test(model, modes, freqs, iterations):
+    fig, ax = plt.subplots(ncols=2)
+    ax[0].set_title("Processed input image")
+    ax[1].set_title("Image reconstruction of predictions")
+    processor = ModeProcessor()
+    for step in range(iterations):
+        for i, mode in enumerate(modes):
+            mode.amplitude = np.sin(freqs[i] * step)
+            mode.phase = 2 * np.pi * np.cos(freqs[i] * step)
+        img = Superposition(*modes).superpose()
+        processed_frame = processor.getImage(img) # Get and process next frame
+        predicted_image = model.predict(processed_frame).superpose()
+        ax[0].imshow(processed_frame)
+        ax[1].imshow(predicted_image)
+        plt.pause(0.05)
+    plt.show()
 
+os.chdir("System")
 fname = r"C:\Users\Tom\Documents\EditedBeamModes.mp4"
-model = ML(BasicGenerator(3, 3, 0.5, 1.0, 0.1, (0.0, 1.0), 32, 64, 128, False), 'Adamax', 0.0001)
-print(os.path.exists("System/Models"))
-print(os.path.exists(f"System/Models/{str(model)}"))
-print(str(model))
+model = ML(BasicGenerator(3, 3, 0.5, 1.0, 0.1, (0.0, 1.0), 32, 64, 128, False), 'Adamax', 0.0001, False) 
 model.load()
 
 dat = visualise_video_predictions(fname, model)
-plt.show()
+#mode_sweep_test(model, [Hermite(0, 0), Hermite(0, 1), Hermite(1, 1), Hermite(1, 0)], [1, 1.5, 0.25, 1/3], 20)
