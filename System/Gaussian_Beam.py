Added models / optimisations and fixed amplitude masking in the loss function

System/Gaussian_Beam.py
@@ -180,9 +180,9 @@ class Hermite:
         '''
         self.phase += phase # Adding extra phase
 
-        if self.phase < -np.pi: # Ensuring phase stays within -π -> π
+        if self.phase <= -np.pi: # Ensuring phase stays within -π -> π
             self.phase += 2*np.pi
-        elif self.phase > np.pi:
+        elif self.phase >= np.pi:
             self.phase -= 2*np.pi
 
 
@@ -429,15 +429,15 @@ class Laguerre(Superposition):
 
     def add_phase(self, phase):
         '''
-        Add phase to superposition, and propagate down to component modes.
+        Adds some phase value to current mode phase
         '''
         self.phase += phase # Adding extra phase
 
-        if self.phase < -np.pi: # Ensuring phase stays within -π -> π
-           self.phase = self.phase % np.pi
-           self.phase += 2*np.pi
-        elif self.phase > np.pi:
-           self.phase = self.phase % -np.pi
+        if self.phase <= -np.pi: # Ensuring phase stays within -π -> π
+            self.phase += 2*np.pi
+        elif self.phase >= np.pi:
+            self.phase -= 2*np.pi
+
         [mode.add_phase(phase) for mode in self.modes] # Propogate phase to constituent modes
 
     def E_mode(self, x, y, z):

System/ML_Identification.py
@@ -144,7 +144,7 @@ class ML:
         '''
         Custom loss function to mask out modes that don't exist in the superposition.
         '''
-        mask = K.cast(K.greater_equal(y_true, 0), K.floatx())
+        mask = K.cast(K.greater(y_true, 0), K.floatx())
         loss = K.square((y_pred * mask) - (y_true * mask))
 
         return K.mean(loss, axis=-1)
@@ -554,7 +554,7 @@ class ML:
         LOG.info("ML object loaded successfully!")
         print("Done!\n")
 
-    def predict(self, data, threshold: float = 0.2, info: bool = True):
+    def predict(self, data, threshold: float = 0.05, info: bool = True):
         '''
         Predict the superposition based on a 2D numpy array of the unknown optical cavity.
         '''
@@ -617,7 +617,7 @@ class ML:
 
         return answer
 
-    def compare(self, sup: Superposition, camera: dict = None, info: bool = True, save: bool = False):
+    def compare(self, sup: Superposition, camera: dict = None, threshold: float = 0.05, info: bool = True, save: bool = False):
         '''
         Plot given superposition against predicted superposition for visual comparison.
         '''
@@ -631,16 +631,17 @@ class ML:
         if info: print(log("[PRED] Actual: " + str(sup)))
         raw_image = sup.superpose()
         noisy_image = processor.errorEffects(raw_image)
-        pred = self.predict(noisy_image, info=info)
+        pred = self.predict(noisy_image, threshold=threshold, info=info)
 
         labels = [str(i) for i in sup]
         sup_amps = [i.amplitude for i in sup]
         pred_amps = [pred.contains(i).amplitude for i in sup]
         sup_phases = [i.phase for i in sup]
-        pred_phases = [pred.contains(i).phase for i in sup]
+        raw_pred_phases = [pred.contains(i).phase for i in sup]
+        pred_phases = [0 if i == -10 else i for i in raw_pred_phases] # If mode does not exist, give it a phase of 0
 
-        x = np.arange(len(labels))  # Label locations
-        width = 0.35  # Width of the bars
+        x = np.arange(len(labels)) # Label locations
+        width = 0.35 # Width of the bars
 
         fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, figsize=(14, 10))
         fig.suptitle(r"$\bf{" + str(self) + "}$")
@@ -660,6 +661,7 @@ class ML:
         rects2 = ax3.bar(x + (width / 2), pred_amps, width, label='Reconstucted', zorder=3)
         rects3 = ax6.bar(x - (width / 2), sup_phases, width, label='Actual', zorder=3)
         rects4 = ax6.bar(x + (width / 2), pred_phases, width, label='Reconstucted', zorder=3)
+        ax3.axhline(threshold, color='r', linestyle='--', zorder=5)
 
         # ax1.colorbar()
         # ax2.colorbar()