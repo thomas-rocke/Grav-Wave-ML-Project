585e422b5c5e579eac4ea279271bd0a3e4797ea5
thomas-rocke <45517493+thomas-rocke@users.noreply.github.com>
2021-03-15 11:06:39 +0000

    Removed capsize on comparison plots, as they are not a handled well across different numbers of modes

--- System/ML_Identification.py
+++ System/ML_Identification.py
@@ -753,9 +753,9 @@ class ML:
         ax4.imshow(sup.phase_map(), cmap='jet')
         ax5.imshow(pred.phase_map(), cmap='jet')
         rects1 = ax3.bar(x - (width / 2), sup_amps, width, label='Actual', zorder=3)
-        rects2 = ax3.bar(x + (width / 2), pred_amps, width, yerr=amp_errs,  label='Reconstucted', zorder=3, capsize=10)
+        rects2 = ax3.bar(x + (width / 2), pred_amps, width, yerr=amp_errs,  label='Reconstucted', zorder=3)
         rects3 = ax6.bar(x - (width / 2), sup_phases, width, label='Actual', zorder=3)
-        rects4 = ax6.bar(x + (width / 2), pred_phases, width, yerr=phase_errs, label='Reconstucted', zorder=3, capsize=10)
+        rects4 = ax6.bar(x + (width / 2), pred_phases, width, yerr=phase_errs, label='Reconstucted', zorder=3)
         ax3.axhline(threshold, color='r', linestyle='--', zorder=5)
 
         # ax1.colorbar()
