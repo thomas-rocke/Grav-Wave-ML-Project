cc65a977561d33cc6c9a323ee9eb282d67afe9a9
Ninstet <jxw1025@student.bham.ac.uk>
2021-03-21 13:44:03 +0000

    Updated Desktop scripts

--- Desktop/All.ps1
+++ Desktop/All.ps1
@@ -6,3 +6,8 @@ conda activate ML-GPU
 & 'C:\Users\Jake\OneDrive - University of Birmingham\Uni\Year 4\Project\Grav-Wave-ML-Project\Desktop\OptimiseRepBatch.ps1'
 & 'C:\Users\Jake\OneDrive - University of Birmingham\Uni\Year 4\Project\Grav-Wave-ML-Project\Desktop\OptimiseRes.ps1'
 & 'C:\Users\Jake\OneDrive - University of Birmingham\Uni\Year 4\Project\Grav-Wave-ML-Project\Desktop\OptimiseVar.ps1'
+
+& 'C:\Users\Jake\OneDrive - University of Birmingham\Uni\Year 4\Project\Grav-Wave-ML-Project\Desktop\OptiBatchFollowup.ps1'
+& 'C:\Users\Jake\OneDrive - University of Birmingham\Uni\Year 4\Project\Grav-Wave-ML-Project\Desktop\OptiLearnFollowup.ps1'
+& 'C:\Users\Jake\OneDrive - University of Birmingham\Uni\Year 4\Project\Grav-Wave-ML-Project\Desktop\OptiResFollowup.ps1'
+& 'C:\Users\Jake\OneDrive - University of Birmingham\Uni\Year 4\Project\Grav-Wave-ML-Project\Desktop\OptiVarFollowup.ps1'
--- Desktop/OptiResFollowup.ps1
+++ Desktop/OptiResFollowup.ps1
@@ -1,3 +1,3 @@
 cd ../../System/
 
-python Main.py -i "ML(SuperpositionGenerator(3, batch_size=8, repeats=8, training_strategy_name='opti_test_strat'), use_multiprocessing=False, optimiser='RMSprop', learning_rate=0.0001)" -o "resolution" "[32, 64, 128, 256, 512]"
+python Main.py -i "ML(SuperpositionGenerator(3, batch_size=8, repeats=8, training_strategy_name='opti_test_strat'), use_multiprocessing=False, optimiser='RMSprop', learning_rate=0.0001)" -o "camera_resolution" "[32, 64, 128, 256, 512]"
